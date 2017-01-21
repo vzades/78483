0e958072b9646a7fe5d52e54f046dce4fa57b0cf
vzades <vinay.komal100@gmail.com>
2017-01-21 18:58:40 +0530

    parallel task

--- 78483/MyTask.cs
+++ 78483/MyTask.cs
@@ -49,21 +49,28 @@ namespace _78483
                 return i.Result * 2;
             });
 
-
+            Console.WriteLine(t2.Result);
             //Child task 
 
             Task<Int32[]> parent = Task.Run(()=> {
                 var results = new Int32[3];
 
                 new Task(()=> results[0]=0,TaskCreationOptions.AttachedToParent).Start();
-                new Task(() => results[0] = 1, TaskCreationOptions.AttachedToParent).Start(); ;
-                new Task(() => results[0] = 2, TaskCreationOptions.AttachedToParent).Start();
+                new Task(() => results[1] = 1, TaskCreationOptions.AttachedToParent).Start(); ;
+                new Task(() => results[2] = 2, TaskCreationOptions.AttachedToParent).Start();
+
 
                 return results;
                
             });
 
-            Console.WriteLine(t2.Result);
+            var finaltask = parent.ContinueWith((s) =>
+            {
+                foreach (int i  in s.Result)
+                {
+                    Console.WriteLine(i);
+                }
+            });
 
             Console.ReadKey();
 
